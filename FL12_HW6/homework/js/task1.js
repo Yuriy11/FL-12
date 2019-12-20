87c8f24fee475d847c6ae2beb5fee4916d0b68e5
Yurii Lampiha <y.lampiga@gmail.com>
2019-12-20 12:36:42 +0200

    mede js input for first task

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,1 +1,8 @@
-// Your code goes here
+let a = parseFloat(prompt("input a fro quadratic equation ax^2 + bx + c = 0"))
+let b = parseFloat(prompt("input a fro quadratic equation ax^2 + bx + c = 0"))
+let c = parseFloat(prompt("input a fro quadratic equation ax^2 + bx + c = 0"))
+console.log(a)
+let discriminant = Math.sqrt(b * b - 4 * a * c)
+if (discriminant < 0) {
+    console.log("no solution")
+}
