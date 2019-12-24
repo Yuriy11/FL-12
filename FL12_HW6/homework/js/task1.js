d28bc208e47945a72726316842a065acb5d80121
Yurii Lampiha <y.lampiga@gmail.com>
2019-12-24 18:54:25 +0200

    once more fixed prompt tips, this time for hw6 task1

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,22 +1,22 @@
 let a = parseFloat(prompt("input a for quadratic equation ax^2 + bx + c = 0"));
-let b = parseFloat(prompt("input a for quadratic equation ax^2 + bx + c = 0"));
-let c = parseFloat(prompt("input a for quadratic equation ax^2 + bx + c = 0"));
+let b = parseFloat(prompt("input b for quadratic equation ax^2 + bx + c = 0"));
+let c = parseFloat(prompt("input c for quadratic equation ax^2 + bx + c = 0"));
 if (isNaN(a) || isNaN(b) || isNaN(c)) {
-  console.log("Invalid input data");
+    console.log("Invalid input data");
 } else {
-  let discriminant = b * b - 4 * a * c;
-  console.log("disc:" + discriminant);
-  if (discriminant < 0) {
-    console.log("no solution");
-  } else if (discriminant === 0) {
-    console.log("disc=0");
-    console.log("x=" + -b / (2 * a));
-  } else {
-    console.log(
-      "x1=" +
-        (-b + Math.sqrt(discriminant)) / (2 * a) +
-        " x2=" +
-        (-b - Math.sqrt(discriminant)) / (2 * a)
-    );
-  }
+    let discriminant = b * b - 4 * a * c;
+    console.log("disc:" + discriminant);
+    if (discriminant < 0) {
+        console.log("no solution");
+    } else if (discriminant === 0) {
+        console.log("disc=0");
+        console.log("x=" + -b / (2 * a));
+    } else {
+        console.log(
+            "x1=" +
+            (-b + Math.sqrt(discriminant)) / (2 * a) +
+            " x2=" +
+            (-b - Math.sqrt(discriminant)) / (2 * a)
+        );
+    }
 }
