820dc0095a336c940a0d9efed4a21a52024e5fac
Yurii Lampiha <y.lampiga@gmail.com>
2019-12-27 16:44:24 +0200

    task 2:  added some user input requirments and responses

--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -9,15 +9,35 @@ if (continueGame) {
     let userPocketChoise;
     let userGuessed = false;
     let userPrize = 0;
+    let userInput;
+    let acceptUserChoise;
     while (continueGame) {
-        currentPocketNumber = Math.round(Math.random() * initialPocketAmount);
+        currentPocketNumber = Math.round(Math.random() * initialPocketAmount * pocketAmountMultiplier);
         do {
+            userInput = null;
+            userPocketChoise = null;
+            acceptUserChoise = false;
+            console.log("Chose a roulette pocket from 0 to " +
+                initialPocketAmount * pocketAmountMultiplier);
             console.log("current attempt : " + currentAttempt + "; pocket number : " + currentPocketNumber);
-            userPocketChoise = parseInt(prompt("Chose a roulette pocket from 0 to " +
-                initialPocketAmount * pocketAmountMultiplier +
-                "\nAttempts left: " + 3 - currentAttempt + " attempts" +
-                "\nTotal prize: " + userPrize + "$" +
-                "\nPossible prize on current attempt: " + prizes[currentAttempt] * prizeMultiplier + "$"));
+            while (!acceptUserChoise) {
+                userInput = prompt("Chose a roulette pocket from 0 to " +
+                    (initialPocketAmount * pocketAmountMultiplier) +
+                    "\nAttempts left: " + (3 - currentAttempt) + " attempts" +
+                    "\nTotal prize: " + userPrize + "$" +
+                    "\nPossible prize on current attempt: " + (prizes[currentAttempt] * prizeMultiplier) + "$");
+                if (userInput !== null) {
+                    if (Number(userInput) <= initialPocketAmount * pocketAmountMultiplier && Number(userInput) >= 0) {
+                        acceptUserChoise = true;
+                        userPocketChoise = Number(userInput);
+                    } else {
+                        alert("Entered value is out of bounds, please try again");
+                    }
+                } else {
+                    alert("Whoops, this chance is lost");
+                    acceptUserChoise = true;
+                }
+            }
             console.log("user choise " + userPocketChoise);
             if (userPocketChoise === currentPocketNumber) {
                 userGuessed = true;
