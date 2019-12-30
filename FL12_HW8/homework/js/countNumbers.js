75e3b430b37212b81391904b165adcc641b9375b
Yurii Lampiha <y.lampiga@gmail.com>
2019-12-30 22:37:57 +0200

    task #5 done

--- FL12_HW8/homework/js/countNumbers.js
+++ FL12_HW8/homework/js/countNumbers.js
@@ -1,9 +1,13 @@
 function countNumbers(inputString) {
-    let result = "";
+    let resultMap = new Map();
     for (let symbol of inputString) {
         if (Number(symbol) >= 0 && Number(symbol) <= 9) {
-            result += symbol;
+            if (resultMap.get(Number(symbol)) === undefined) {
+                resultMap.set(Number(symbol), 1);
+            } else {
+                resultMap.set(Number(symbol), resultMap.get(Number(symbol)) + 1);
+            }
         }
     }
-    return result;
+    return resultMap;
 }
