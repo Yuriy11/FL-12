ca64be4ef96594326fea6e43453d701a6851d5dc
Yurii Lampiha <y.lampiga@gmail.com>
2020-01-12 19:53:36 +0200

    HW9 tasks 1 2 3 5 6 completed

--- FL12_HW9/homework/index.js
+++ FL12_HW9/homework/index.js
@@ -1,1 +1,54 @@
 // Your code goes here
+
+function convert(...elements)
+{
+    result=[];
+    for (let element of elements){
+        if(typeof(element)==="string"){
+            result.push(parseInt(element));
+        }
+        else if(typeof(element)==="number"){
+            result.push(element.toString())
+        }
+        else{
+            result.push(element)
+        }
+    }
+    return result;
+}
+
+function executeforEach(array,func)
+{
+    for(let i=0;i<array.length;i++){
+        array[i]=func(array[i]);  
+    }
+}
+
+function mapArray(array,func)
+{
+    let _array=[];
+    for(let element of array)
+    {
+        _array.push(parseInt(element));
+    }
+    executeforEach(_array,func);
+    return _array;
+}
+
+function flipOver(string){
+    result="";
+    for(let i=string.length-1;i>=0;i--){
+        result+=string[i];
+    }
+    return result;
+}
+function makeListFromRange(array){
+    result=[];
+    for(let i=array[0];i<=array[1];i++)
+    {
+        result.push(i);
+    }
+    return result;
+}
+
+getArrayOfKeys(objects,)
