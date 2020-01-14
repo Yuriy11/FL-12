ce215529a5780a41c59320b60bda30f99374136f
Yurii Lampiha <y.lampiga@gmail.com>
2020-01-14 23:10:36 +0200

    all tasks finished, errors fixed

--- FL12_HW9/homework/index.js
+++ FL12_HW9/homework/index.js
@@ -1,53 +1,98 @@
 // Your code goes here
 
-function convert(...elements)
-{
-    result=[];
-    for (let element of elements){
-        if(typeof(element)==="string"){
+function convert(...elements) {
+    let result = [];
+    for (let element of elements) {
+        if (typeof element === "string") {
             result.push(parseInt(element));
-        }
-        else if(typeof(element)==="number"){
+        } else if (typeof element === "number") {
             result.push(element.toString())
-        }
-        else{
+        } else {
             result.push(element)
         }
     }
     return result;
 }
 
-function executeforEach(array,func)
-{
-    for(let i=0;i<array.length;i++){
-        array[i]=func(array[i]);  
+function executeforEach(array, func) {
+    for (let i = 0; i < array.length; i++) {
+        array[i] = func(array[i]);
     }
 }
 
-function mapArray(array,func)
-{
-    let _array=[];
-    for(let element of array)
-    {
+function mapArray(array, func) {
+    let _array = [];
+    for (let element of array) {
         _array.push(parseInt(element));
     }
-    executeforEach(_array,func);
+    executeforEach(_array, func);
     return _array;
 }
+let a = [1, 2, 3, 4];
+function filterArray(array, func) {
+    let result = [];
+    executeforEach(array, function (el) {
+        if (func(el)) {
+            result.push(el);
+        }
+    });
+    return result;
+}
 
-function flipOver(string){
-    result="";
-    for(let i=string.length-1;i>=0;i--){
-        result+=string[i];
+function flipOver(string) {
+    let result = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+        result += string[i];
     }
     return result;
 }
-function makeListFromRange(array){
-    result=[];
-    for(let i=array[0];i<=array[1];i++)
-    {
+
+function makeListFromRange(array) {
+    let result = [];
+    for (let i = array[0]; i <= array[1]; i++) {
         result.push(i);
     }
     return result;
 }
 
+const actors = [
+    { name: "tommy", age: 36 },
+    { name: "lee", age: 28 }
+];
+
+function getArrayOfKeys(objects, keyName) {
+    let _objects = objects;
+    executeforEach(_objects, function (el) { 
+        return el[keyName];
+    });
+    return _objects;
+}
+
+function substitute(arr) {
+    let substituteSubFun=function (el) {
+        if (el < 30) {
+            return "*";
+        } else {
+            return el;
+        }
+    }
+    let _arr = mapArray(arr, substituteSubFun);
+    return _arr;
+}
+const custDate = new Date('6/15/2018 09:14:00');
+function getPastDay(date, daysBefore) {
+    return new Date(date.getTime() - daysBefore * 86400000);
+}
+
+function formatDate(date) {
+    let addZero = function (val) {
+        if (val < 10) {
+            return `0${val}`
+        }
+        return val;
+    }
+    return date.getFullYear().toString() + "/" + (date.getMonth() + 1).toString() + 
+    "/" + date.getDate().toString() + " " +
+        addZero(date.getHours()).toString() + ":"
+         + addZero(date.getMinutes()).toString();
+}
